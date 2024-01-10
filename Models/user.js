efa98ac1f6c178911db17675e3427a75562ea06e
suyeon <jso8787@naver.com>
2024-01-10 15:06:18 +0900

    데이터베이스 세팅

---
+++ Models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema(
+    { imageUrl: String },
+    { timeStamp : true}
+)
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	gallery: {
+		images: [imageSchema]
+	},
+    online: {
+        type: Boolean,
+        dafault: false
+    }
+})
+
+const User = new mongoose.Model('User', userSchema);
+
+module.exports = User;
