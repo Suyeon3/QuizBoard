13d74bfc082195f26bf6a3b54d4eeb568e6018f3
suyeon <jso8787@naver.com>
2024-01-10 17:18:34 +0900

    백엔드 웹소켓 연동

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect(process.env.DB)
     .catch(error => handleError(error))
     .then(() => console.log('connected to database'));
 
